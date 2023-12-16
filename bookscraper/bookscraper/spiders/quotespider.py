04ff9dcde35c204771952ed0184cee7ce57a490d
Burak <burakayan760@gmail.com>
2023-12-16 13:45:23 +0300

    quote ları arraye alıyo

--- bookscraper/bookscraper/spiders/quotespider.py
+++ bookscraper/bookscraper/spiders/quotespider.py
@@ -4,6 +4,7 @@ class QuotespiderSpider(scrapy.Spider):
     name = "quotespider"
     allowed_domains = ["quotes.toscrape.com"]
     start_urls = ["http://quotes.toscrape.com/"]
+    quote_list = []  
 
     def parse(self, response):
         quotes = response.css('.quote')
@@ -11,6 +12,12 @@ class QuotespiderSpider(scrapy.Spider):
         for quote in quotes:
             text = quote.css('.text::text').get()   
             yield {
-                'text': text,
-            }
+                'quote': text,
+            }    
+            self.quote_list.append({'quote': text})  
+        for element in self.quote_list:
+            print(element)
+
+
+
 
